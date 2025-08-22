bfea68a264f708c232336f5a148de64f2b7b7358
Ubuntu <ubuntu@ip-172-31-21-221.ec2.internal>
2025-08-22 13:18:26 +0000

    update

--- hello1.sh
+++ hello1.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-echo "hello naman"
+echo "hello buddy"
 
+#vatiables*****
+name="naman"
+echo "my name is $name"
+# $ is use to take a info from variable name
 
+echo "enter your username:"
+read username            #this is read keyword you help in write in commandline
+echo "you enter $username"
+
+#argument*****
+echo "the names in $name life $0 is $1 and $2"          # ./hello1.sh aman tushar  if you run this your argument pass
